e190a0a0590dcd305e565148ff9f0ebd283a7a15
AmraniSaad <x544d3v@gmail.com>
2020-08-15 12:56:43 +0000

    Schedule Commit

--- tools/parser/_lexer_/__c.py
+++ tools/parser/_lexer_/__c.py
@@ -7,7 +7,7 @@
 #----------------------------------------------------------------------------------------#
 
 
-class __C(object):
+class Cfg(object):
     """+ Will Check and Load cfg file
     + Parse the content to bytes (short)
     + Get them ready along with the file content to be sent to the extern function
@@ -21,19 +21,19 @@ class __C(object):
 
             for i,line in enumerate(self.cfg):
                 # flag must start with either _ or .
-                if line[0] in ['._']:
+                if line[0] in '_.':
                     splited_line = line.split(' ')
                     
                     assert splited_line.__len__() == 2 , "[ERR] File __c.cfg is Broken !" #f_call_fixer
                     self.maped.append(splited_line[0]+chr(0x0))
-                    _tmp_len = splited_line[1].__len__()
+                    _tmp_len = splited_line[1].__len__() - 1
 
                     assert _tmp_len > 0 , "[ERR] File __c.cfg is Broken !" #f_call_fixer
-                    assert _tmp_len % 2 == 0 , "[ERR] File __c.cfg is Broken !" #f_call_fixer
+                    assert _tmp_len  % 2 == 0 ,"[3ERR] File __c.cfg is Broken !" #f_call_fixer
 
                     pos = 0
                     while pos < _tmp_len:
-                        self.maped[i] += chr(int(splited_line[1][pos]+splited_line[1][pos+1]))
+                        self.maped[i] += chr(int(splited_line[1][pos]+splited_line[1][pos+1], 16))
                         pos += 2  
 
 
--- tools/parser/_lexer_/lexer.py
+++ tools/parser/_lexer_/lexer.py
@@ -1,15 +1,15 @@
-__import__('sys').path.append('..') # Get rid of higher dir msg
-from __c import __C
+#__import__('sys').path.append('..') # Get rid of higher dir msg
+from __c import Cfg
 
 class Lexer(object):
 
     def __init__(self , abs_file_path):
         print("+ Starting Lexer ")
-        self.maped_cfg_instance = __C()
+        self.maped_cfg_instance = Cfg()
         self.maped_cfg_value    = self.maped_cfg_instance.get_cfg()
         self.maped_cfg_len = self.maped_cfg_value.__len__()
 
-        assert self.maped_cfg_len > 0 , "[ERR] cfg File Returned Null .. !"
+        #assert self.maped_cfg_len > 0 , "[ERR] cfg File Returned Null .. !"
 
         try:
             for line in self.maped_cfg_value:
