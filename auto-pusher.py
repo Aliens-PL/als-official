cde3e574570f81e0e231acd08a79b8ca005af326
Amrani <x544d3v@gmail.com>
2020-08-14 21:52:39 -0700

    'From_AP'

--- auto-pusher.py
+++ auto-pusher.py
@@ -8,10 +8,13 @@ _PUSH_EACH  = 3600 # 1h
 _PUSH_ADD   = '.'
 _PUSH_BRNCH = 'saad'
 
+cnt = 0
 
 while True:    
     system("git add "+_PUSH_ADD)
     system("git commit -m 'From_AP'")
-    system("git push origin "+_PUSH_BRANCH)
-
+    system("git push origin "+_PUSH_BRNCH)
+    cnt += 1
+    print("\n----------------[ PUSH NBR "+str(cnt)+" ]----------------\n")
     sleep(_PUSH_EACH)
+
