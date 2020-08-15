4579df55acf39525ff070ca01b3d24e38f4297d9
Amrani <x544d3v@gmail.com>
2020-08-15 02:31:31 -0700

    'From_AP'

--- tools/parser/_lexer_/__c.py
+++ tools/parser/_lexer_/__c.py
@@ -3,4 +3,10 @@
 class __c:
 
     def __init__(self):
+        try:
+            self.cfg = open("__c.cfg").read()
+            assert self.cfg != None , "[ERR] File __c.cfg is Missing or Messed up !"
+            self.mapped = [for ]
+        except Exception as ex:
+            print(str(ex))
         
