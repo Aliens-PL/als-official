f3a247aa1f3b7548e28b33dd051b076c633198ec
Amrani <x544d3v@gmail.com>
2020-08-15 06:09:29 -0700

    Schedule Commit

--- tools/parser/_lexer_/__c.py
+++ tools/parser/_lexer_/__c.py
@@ -13,6 +13,7 @@ class Cfg(object):
     + Get them ready along with the file content to be sent to the extern function
     """
     def __init__(self):
+        flag_sep = 0x01
         print("+ Starting __C ")
         self.maped = []
         try:
@@ -21,15 +22,15 @@ class Cfg(object):
 
             for i,line in enumerate(self.cfg):
                 # flag must start with either _ or .
-                if line[0] in '_.':
+                if line[0] in ['.', '_']:
                     splited_line = line.split(' ')
                     
                     assert splited_line.__len__() == 2 , "[ERR] File __c.cfg is Broken !" #f_call_fixer
-                    self.maped.append(splited_line[0]+chr(0x0))
-                    _tmp_len = splited_line[1].__len__() - 1
+                    self.maped.append(splited_line[0]+chr(flag_sep))
+                    _tmp_len = splited_line[1].replace('\n','').__len__()
 
                     assert _tmp_len > 0 , "[ERR] File __c.cfg is Broken !" #f_call_fixer
-                    assert _tmp_len  % 2 == 0 ,"[3ERR] File __c.cfg is Broken !" #f_call_fixer
+                    assert _tmp_len % 2 == 0 ,str(_tmp_len)+"[ERR] File __c.cfg is Broken !" #f_call_fixer
 
                     pos = 0
                     while pos < _tmp_len:
@@ -43,6 +44,7 @@ class Cfg(object):
 
         except Exception as ex:
             print(str(ex))
+            exit(0)
     
     def get_cfg(self):
         return self.maped
