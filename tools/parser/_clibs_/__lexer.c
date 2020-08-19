8be8ea12bfe21dce66198ba494f367f8917008f0
Amrani <x544d3v@gmail.com>
2020-08-18 19:04:07 -0700

    Schedule Commit

--- tools/parser/_clibs_/__lexer.c
+++ tools/parser/_clibs_/__lexer.c
@@ -1,21 +1,43 @@
+#include "__lexer.h"
 #include <stdio.h>
-#define np (void *)0
+#include "string.h"
+
+#define NP (void *)0
+
 
 void * Lexer(const char * cfg , const char * src )
 {
-    while (*cfg != '\0')
+    //
+    return NP;
+}
+
+
+
+void * check_loads(const char * line)
+{
+    /*
+     *  Possible cases for loading :
+     *  1- load ModuleName
+     *  2- from 'http://....../file.als' load ModuleName
+     *  3- from 'path_to_file/file.als' load ModuleName
+     *  4- load @base
+     *  5- from @base load @convert
+    */
+    
+    // check if valid loading
+    // Hardcoded indexes!
+    if ((*strstr(line, *_ALS_MODULE[0]) | *strstr(line, *_ALS_MODULE[1]) | *strstr(line, *_ALS_MODULE[2])) == NULL)
     {
-        puts(cfg);
-        cfg++;
+        
     }
     
-    printf("\n\n");
 
-    while (*src != '\0')
+    size_t _len_ = strlen(line);
+
+
+    for (size_t i = 0; i < _len_ ; i++)
     {
-        puts(src);
-        src++;
+        
     }
-
-    return np;
+    
 }
--- tools/parser/_clibs_/__lexer.h
+++ tools/parser/_clibs_/__lexer.h
@@ -1,6 +1,195 @@
 #ifndef lexer_h__
 #define lexer_h__
+#include <stdlib.h>
 
+
+// ------------ COMFIGS ------------ //
+#define MAX_POSSIBLE_MODULES_LOADS 0xB
+
+
+// -----------    END    ----------- //
+
+
+// BASIC PROGRAMMING
+
+#define ALS_MODULE      "_A000"
+#define ALS_CONDITION   "_A001"
+#define ALS_OPERATION   "_A002"
+#define ALS_LOOP        "_A003"
+#define ALS_KEYWORD     "_A004"
+#define ALS_PREFS_F     "_A005"
+#define ALS_PREFS_C     "_A006"
+#define ALS_SYMBOLE_1   ".A007"
+#define ALS_SYMBOLE_2   ".A008"
+#define ALS_FUNCTION    "_A009"
+#define ALS_VARIABLE    "_A010"
+#define ALS_F_PARAM     "_A011"
+
+// O O P -
+
+#define ALS_PLANET              "_B000"
+#define ALS_CONSTRUCTOR         "_B001"
+
+#define ALS_IPLANET             "_B002"
+#define ALS_IPLANET_ABS_METHODE "_B003"
+
+// ------------   TYPE DEFS   ------------ //
+
+typedef struct __LOADS
+{
+    char * path = NULL;
+    char * Modules[MAX_POSSIBLE_MODULES_LOADS];
+
+} LEX_LOADS;
+
+
+typedef struct __TOKEN
+{
+    int child_of = 0;
+    char * type;
+    char * value;
+
+} LEX_TOKEN;
+
+
+typedef struct __LINE
+{
+    int number;
+    char * startType;
+    char * value;
+
+} LEX_LINE;
+
+// ------------ CASES ------------ //
+
+const char * _ALS_MODULE[] = 
+{ 
+    "from",
+    "extern"
+    "load",
+};
+
+const char * _ALS_CONDITION[] = 
+{ 
+    "?"     ,"??"   ,"if",
+    ":"     ,"and"  ,"or",
+    "=="    ,"&&"   ,"||",
+    "in"
+};
+
+const char * _ALS_OPERATION[] = 
+{ 
+    "+"     ,"-"    ,"*",
+    "/"     ,"%"    ,"=",
+    ">>"    ,"<<"   ,"|",
+    "^"     ,"&"    ,"~"
+};
+
+const char * _ALS_LOOP[] = 
+{ 
+    "while" ,"loop"
+};
+
+const char * _ALS_KEYWORD[] = 
+{ 
+    "ZM" ,      "null",     "space",
+    "galaxy",   "planet",   "iplanet",
+    "not",      "null",     "imported"
+};
+
+const char * _ALS_PREFS_F[] =
+{
+    // Coverting Funcs
+    "tonumber",     "totext",       "tojson",
+    "toxml",        "tobin",        "toascii",
+    "todict",       "tolist",
+    
+    // Type Checking Funcs
+    "istext",       "isdate",       "istime",
+    "isnull",       "isequal",      "ismatch",
+
+    // List basic Functions
+    "add",          "insert",       "update",
+    "remove",       "sort",         "reverse",
+    "pick",         "find",         "size",
+    "clear"
+
+    // Math Functs -- this is better if we check only if loaded math module
+    "sqrt",         "abs",          "pow",
+    "fact",         "log",          "exp",
+    "sin",          "cos",          "tan",
+    "asin",         "acos",         "atan",
+    "rand",         
+
+    // date and time funcs
+    "date",         "time",         "adddays",
+    "diffdays",     "months",       "days",
+    "years",        "hours",        "minutes",
+    "seconds",
+
+    // Strings Functions
+    "lower",        "upper",        "split",
+    "len",          "contains",     "inbetween",    
+    "replace",      "slice",        "count"
+
+    // Files Functions
+    "f_extension",  "f_size",       "f_name",
+    "f_delete",     "f_copy",       "f_move",
+    "f_read",       "f_write",      "f_append",
+    "f_loop_lines", "f_loop_bytes"
+
+    // Global Use funcs
+    "typeof",       "nameof",       "system",
+    "als_test",
+
+};
+
+
+// constants starts with _$NAME (UPPERCASE)
+
+const char * _ALS_PREFS_C[] = 
+{ 
+    "ALPHABETS_UPPER",      "ALPHABETS_LOWER",      "ASCII_TABLE",
+    "WORD_SIZE",            "DWORD_SIZE",           "QWORD_SIZE",
+    "NEW_LINE",             "ALS_VERSION",          "GITHUB_REPO",
+    "SPONSOR_LINK",         "WEBSITE"
+};
+
+// Symboles that does not need opening-closing
+const unsigned char _ALS_SYMBOLE_1[] = 
+{
+    0x40,   0x23,   0x21,   // @ # !
+    0x2c,   0x5f,   0x24,   // , _ $
+    0x2e,                   // .
+
+};
+
+
+// the opening is always in an index fardi
+// the closing is always in an index zawji
+const unsigned char _ALS_SYMBOLE_1[] = 
+{
+    0x7b,   0x7d,   // {}
+    0x5b,   0x5d,   // []
+    0x28,   0x29,   // ()
+    0x27,   0x27,   // ''
+    0x22,   0x22    // ""
+};
+
+
+
+/*
+ *      ---- START ----
+ *  SOME COOL PRESENTATIONS
+ *  AND EXPLAINATIONS ...ETC
+ *  SO THE USERS UNDERSTAND
+ *      ---- END   ----
+ */
+
+
+// Main Lexer Func
 extern void * Lexer(const char *, const char *);
+void * check_loads(const char * line);
+
 
 #endif
