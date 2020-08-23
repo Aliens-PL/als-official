935d463fe06760fd4e5351cbd810ba09dbd64b2c
facecheckdisnuts <omranimahmoud@gmail.com>
2020-08-23 12:54:54 +0100

    somme changes

--- Web_module/Test.js
+++ Web_module/Test.js
@@ -1,6 +1,16 @@
 var fs = require('fs');
 
 
+// for the moment only one file css and js 
+const generate_document_header = (title,css_file,js_script)=>{
+    return `<head>
+                <meta charset="utf-8">
+                <title>${title}</title>
+                <link rel="stylesheet" href="${css_file}.css">
+                <script src="${js_script}"></script>
+            </head>`
+}
+
 const generate_content_display = (elements,className)=>{
 
         return (`<div class='${className}'>${return_items_to_render(elements)}</div>`)
@@ -15,13 +25,13 @@ const generate_headline = (variant,className,headline_text,isBold)=>{
         return (`<h${variant} class='${className}'>${isBold?'<b>'+headline_text+'</b>':headline_text}</h${variant}>`)
 }
 
-const generate_default_container_ALS = (elements)=>{
-    return (`<div class='container'>${elements}</div>`)
+const generate_default_container_ALS = (page_title,style,js,elements)=>{
+    return (`<html>${generate_document_header(page_title,style,js)}<body><div class='container'>${return_items_to_render(elements)}</div></body></html>`)
 }
 
 
 const generate_image = (source,width,hiegth,className)=>{
-    return (`<img  ${className?"class='"+className+"'":null} src='${source}' width='${width}px' hiegth='${hiegth}px' />`)
+    return (`<img  ${className?"class='"+className+"'":''} src='${source}' width='${width}px' hiegth='${hiegth}px' />`)
 }
 
 const generate_list_elements = (array)=>{
@@ -53,9 +63,12 @@ return to_render
 }
  const p = generate_paragraphe('try','try');
  const dropdown = generate_list(['hello','ALS','111']);
-// const img = generate_image('https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRtuJsCJezdhm0DRmP3cDTVUDZdrKn_uzS5IrMQeT-3VlK52LJT',100,100);
-//const headline = generate_headline(3,'test','hello');
- const dipslay = generate_content_display([p,dropdown],'container')
+ const img = generate_image('https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRtuJsCJezdhm0DRmP3cDTVUDZdrKn_uzS5IrMQeT-3VlK52LJT',100,100);
+const headline = generate_headline(3,'test','hello');
+
+const div_1 = generate_content_display([p,dropdown],'div_1_style');
+const div_2 = generate_content_display([img,headline],'div_2_style')
+ const dipslay = generate_default_container_ALS('mah_test','style','js',[div_1,div_2])
  fs.writeFile('./myPage.html',dipslay,(error) => { 
 
     console.log('error',error)
