f80afdaa0e9af2b015f0b3da335069a305aca26f
AMRANI SAAD <62079074+x544D@users.noreply.github.com>
2020-08-31 23:11:39 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 echo "
-+----------------- ALS-WELCOME------------------+
-- 	Made By 0x544D, ADIL, JAMAL
--	github : Aliens-PL/als-official
-+-----------------------------------------------+
+          _      _____ ______ _   _  _____            _____  _      
+    /\   | |    |_   _|  ____| \ | |/ ____|          |  __ \| |     
+   /  \  | |      | | | |__  |  \| | (___    ______  | |__) | |     
+  / /\ \ | |      | | |  __| | . ` |\___ \  |______| |  ___/| |      
+ / ____ \| |____ _| |_| |____| |\  |____) |          | |    | |____ 
+/_/    \_\______|_____|______|_| \_|_____/           |_|    |______| v0.1
 
-+ Usage : als [-help] [createproject] [run] [update] [remove]	
-+ NOTE : this project is still in alpha test, so we expect a lot of bugs reports from you :) .
++ Github    : https://github.com/AliensPL/als-official
++ Made by   : @samoray1998 , @AdilMERZ , @x544D 
++ Note      : This is still not even fully functional , the main idea of this is,
+              To Provide Our Idea , we Had litteraly no time to finish all this ,
+              Since we only worked as 3 of us , But we will keep pushing it to the top :D .
+
++ Usage : als help
 
 "
 
-scr_dir=$(dirname "$(readlink -f "$BASH_SOURCE")")/tools/parser/
+scr_dir=$(dirname "$(readlink -f "$BASH_SOURCE")")/tools/
 
 
 chmod 777 $scr_dir'als.py'
